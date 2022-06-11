f758e604bc243848a9a6c8e612afc12ee24fb44b
Roman Vasilyev <roman.vasilev@ottofeller.com>
2022-06-11 18:03:45 +0300

    Add codegen config

--- src/github/pull-request-test.ts
+++ src/github/pull-request-test.ts
@@ -3,7 +3,7 @@ import {Component, github as gh} from 'projen'
 /**
  * Options for PullRequestLint
  */
-export interface PullRequestLintOptions {
+export interface PullRequestTestOptions {
   /**
    * Github Runner selection labels
    * @default ['ubuntu-latest']
@@ -15,8 +15,8 @@ export interface PullRequestLintOptions {
  * Configure validations to run on GitHub pull requests.
  * Only generates a file if at least one linter is configured.
  */
-export class PullRequestLint extends Component {
-  constructor(github: gh.GitHub, options: PullRequestLintOptions = {}) {
+export class PullRequestTest extends Component {
+  constructor(github: gh.GitHub, options: PullRequestTestOptions = {}) {
     super(github.project)
 
     const job = (steps: Array<gh.workflows.JobStep>): gh.workflows.Job => ({
