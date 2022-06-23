fef81efc92fe4c49853ffe7870a9d846556d8d6e
Artem Rudenko <gvidon@ottofeller.com>
2022-06-23 11:33:19 +0200

    Move VSCode application to the static function
    Out of standalone function.

--- src/common/vscode-settings/index.ts
+++ src/common/vscode-settings/index.ts
@@ -22,44 +22,42 @@ export class VsCodeSettings extends JsonFile {
   add(settings: Record<string, unknown>) {
     deepMerge([this.settings, settings], true)
   }
-}
 
-export function configureVSCode(project: NodeProject) {
-  // Settings file
-  new VsCodeSettings(project, {
-    '[dockerfile]': {
-      'editor.defaultFormatter': 'ms-azuretools.vscode-docker',
-    },
-    'typescript.tsdk': 'node_modules/typescript/lib',
-    'path-autocomplete.extensionOnImport': true,
-    'json.schemaDownload.enable': true,
-  })
+  static addToProject(project: NodeProject) {
+    // Settings file
+    new VsCodeSettings(project, {
+      '[dockerfile]': {'editor.defaultFormatter': 'ms-azuretools.vscode-docker'},
+      'typescript.tsdk': 'node_modules/typescript/lib',
+      'path-autocomplete.extensionOnImport': true,
+      'json.schemaDownload.enable': true,
+    })
 
-  // Recommended extensions
-  new JsonFile(project, '.vscode/extensions.json', {
-    newline: true,
-    obj: {
-      recommendations: [
-        'exodiusstudios.comment-anchors',
-        'mikestead.dotenv',
-        'dbaeumer.vscode-eslint',
-        'mohsen1.prettify-json',
-        'github.copilot',
-        'graphql.vscode-graphql',
-        'bradlc.vscode-tailwindcss',
-        'esbenp.prettier-vscode',
-        'github.vscode-pull-request-github',
-        'streetsidesoftware.code-spell-checker',
-        'orta.vscode-jest',
-        'amazonwebservices.aws-toolkit-vscode',
-        'ms-azuretools.vscode-docker',
-        'Quidgest.vscode-velocity',
-      ],
-      unwantedRecommendations: [
-        'DavidAnson.vscode-markdownlint',
-        'GoogleCloudTools.cloudcode',
-        'ms-kubernetes-tools.vscode-kubernetes-tools',
-      ],
-    },
-  })
+    // Recommended extensions
+    new JsonFile(project, '.vscode/extensions.json', {
+      newline: true,
+      obj: {
+        recommendations: [
+          'exodiusstudios.comment-anchors',
+          'mikestead.dotenv',
+          'dbaeumer.vscode-eslint',
+          'mohsen1.prettify-json',
+          'github.copilot',
+          'graphql.vscode-graphql',
+          'bradlc.vscode-tailwindcss',
+          'esbenp.prettier-vscode',
+          'github.vscode-pull-request-github',
+          'streetsidesoftware.code-spell-checker',
+          'orta.vscode-jest',
+          'amazonwebservices.aws-toolkit-vscode',
+          'ms-azuretools.vscode-docker',
+          'Quidgest.vscode-velocity',
+        ],
+        unwantedRecommendations: [
+          'DavidAnson.vscode-markdownlint',
+          'GoogleCloudTools.cloudcode',
+          'ms-kubernetes-tools.vscode-kubernetes-tools',
+        ],
+      },
+    })
+  }
 }
