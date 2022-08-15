ae3a4c77f62444f1fa28507ad6454d58a7dd4f7b
Artem Rudenko <gvidon@ottofeller.com>
2022-08-15 17:04:54 +0200

    Document new functions

--- src/common/github/job.ts
+++ src/common/github/job.ts
@@ -1,7 +1,15 @@
 import * as projen from 'projen'
 import {JobStep} from 'projen/lib/github/workflows-model'
 
-export const job = (steps: Array<JobStep>) => ({
+/**
+ * Create basic GitHub workflow job.
+ */
+export const job = (
+  /**
+   * An array of one or more steps.
+   */
+  steps: Array<JobStep>,
+) => ({
   runsOn: ['ubuntu-latest'],
   permissions: {contents: projen.github.workflows.JobPermission.READ},
   steps,
--- src/common/github/npm-run-job.ts
+++ src/common/github/npm-run-job.ts
@@ -1,4 +1,12 @@
-export const npmRunJob = (command: string) => ({
+/**
+ * Create GitHub workflow which runs npm script using ottofeller/github-actions/npm-run@main action.
+ */
+export const npmRunJob = (
+  /**
+ * npm script command. E.g. command === "build" will result in "npm run build"
+ */
+  command: string,
+) => ({
   uses: 'ottofeller/github-actions/npm-run@main',
   with: {'node-version': 16, command: `npm run ${command}`},
 })
