dbbb74018876d29f99e310cc163c978dbc9ef5c7
RomanVasilyev <roman.vasilev@ottofeller.com>
2022-11-18 19:29:32 +0300

    Wrap ofmt inputs with quates
    This way ofmt can be called once on multiples dirs.

--- src/common/lint/add-lint-scripts.ts
+++ src/common/lint/add-lint-scripts.ts
@@ -1,10 +1,8 @@
 import {NodeProject} from 'projen/lib/javascript'
 
 export const addLintScripts = (project: NodeProject, lintPaths: Array<string>): void => {
-  project.setScript('format', lintPaths.map((lintPath) => `ofmt ${lintPath}`).join(' && '))
+  const joinedPaths = lintPaths.join(' ')
+  project.setScript('format', `ofmt "${joinedPaths}"`)
   project.setScript('typecheck', 'tsc --noEmit --project tsconfig.dev.json')
-
-  // TODO Update to a single call with multiple inputs when the ofmt package is able to handle it.
-  const ofmtLint = lintPaths.map((lintPath) => `ofmt --lint ${lintPath}`).join(' && ')
-  project.setScript('lint', `${ofmtLint} && olint ${lintPaths.join(' ')}`)
+  project.setScript('lint', `ofmt --lint "${joinedPaths}" && olint ${joinedPaths}`)
 }
