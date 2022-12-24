96167b11df8329ac3ed33d6b14ad3bc632a815bd
Roman Vasilyev <roman.vasilev@ottofeller.com>
2022-12-24 18:16:05 +0300

    Move sample code and ui packages setup into functions, hide ui-related stuff under a flag, add @next/fonts and headlessui

--- src/nextjs/assets/pages/_app.tsx
+++ src/nextjs/assets/pages/_app.tsx
@@ -1,4 +1,3 @@
-import 'assets/global.css'
 import {AppProps} from 'next/app'
 
 export default function App({Component, pageProps}: AppProps) {
---
+++ src/nextjs/setup-ui-packages.ts
@@ -0,0 +1,41 @@
+/* eslint-disable import/no-relative-parent-imports -- JSII project rewrites tsconfig thus always overriding introduced aliases */
+import * as path from 'path'
+import * as projen from 'projen'
+import type {OttofellerNextjsProject, OttofellerNextjsProjectOptions} from '.'
+import {AssetFile} from '../common'
+
+/**
+ * Check options for UI flag and setup the required packages in the project. Add corresponding configs.
+ */
+export function setupUIPackages(
+  project: OttofellerNextjsProject,
+  options: OttofellerNextjsProjectOptions,
+  assetsDir: string,
+) {
+  const includeUIPackages = options.ui ?? true
+
+  if (!includeUIPackages) {
+    return
+  }
+
+  project.addDeps(
+    'postcss',
+    'tailwindcss',
+    'autoprefixer',
+    '@tailwindcss/line-clamp',
+    '@tailwindcss/container-queries',
+    '@next/font',
+    '@headlessui/react',
+  )
+
+  new projen.JsonFile(project, 'postcss.config.json', {
+    obj: {plugins: {'tailwindcss/nesting': {}, tailwindcss: {}, autoprefixer: {}}},
+    marker: false,
+  })
+
+  new AssetFile(project, 'tailwind.config.defaults.js', {
+    sourcePath: path.join(assetsDir, 'tailwind.config.defaults.js'),
+  })
+
+  new projen.SampleFile(project, 'tailwind.config.js', {sourcePath: path.join(assetsDir, 'tailwind.config.js')})
+}
