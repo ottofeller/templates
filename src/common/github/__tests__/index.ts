6a7ed0cb7d1e966e967a070cde7c3294de4e2e36
RomanVasilyev <roman.vasilev@ottofeller.com>
2023-01-16 18:54:03 +0300

    Add github utils tests

---
+++ src/common/github/__tests__/index.ts
@@ -0,0 +1,116 @@
+import * as projen from 'projen'
+import {NodeProject, NodeProjectOptions} from 'projen/lib/javascript'
+import {synthSnapshot} from 'projen/lib/util/synth'
+import * as YAML from 'yaml'
+import {job, npmRunJob, PullRequestTest, ReleaseWorkflow} from '..'
+
+describe('github utils', () => {
+  test('job function creates a job object', () => {
+    const steps = [
+      {name: 'first step', run: 'node firstStep.js'},
+      {name: 'second step', run: 'node secondStep.js'},
+    ]
+
+    const jobObject = job(steps)
+
+    expect(jobObject).toStrictEqual({
+      runsOn: ['ubuntu-latest'],
+      permissions: {contents: 'read'},
+      steps,
+    })
+  })
+
+  test('npmRunJob function creates a job object', () => {
+    const command = 'command'
+    const jobObject = npmRunJob(command)
+
+    expect(jobObject).toStrictEqual({
+      uses: 'ottofeller/github-actions/npm-run@main',
+      with: {'node-version': 16, command: `npm run ${command}`},
+    })
+  })
+
+  describe('PullRequestTest', () => {
+    const testWorkflowPath = '.github/workflows/test.yml'
+
+    test('creates a test workflow', () => {
+      const project = new TestProject()
+      new PullRequestTest(project.github!)
+      const snapshot = synthSnapshot(project)
+      expect(snapshot).toMatchSnapshot()
+      expect(snapshot[testWorkflowPath]).toBeDefined()
+    })
+
+    test('adds basic checks to the test workflow', () => {
+      const project = new TestProject()
+      new PullRequestTest(project.github!)
+      const snapshot = synthSnapshot(project)
+      const workflow = YAML.parse(snapshot[testWorkflowPath])
+
+      const jobs = Object.values<projen.github.workflows.Job>(workflow.jobs).map((j) => j.steps[0].with!.command)
+      expect(jobs).toHaveLength(3)
+      expect(jobs).toContain('npm run typecheck')
+      expect(jobs).toContain('npm run lint')
+      expect(jobs).toContain('npm run test')
+    })
+
+    test('allows runsOn override', () => {
+      const project = new TestProject()
+      const runsOn = ['test-env1', 'test-env2']
+      new PullRequestTest(project.github!, {runsOn})
+      const snapshot = synthSnapshot(project)
+      const workflow = YAML.parse(snapshot[testWorkflowPath])
+
+      Object.values<Record<string, unknown>>(workflow.jobs).forEach((j) => {
+        expect(j['runs-on']).toEqual(runsOn)
+      })
+    })
+  })
+
+  describe('ReleaseWorkflow', () => {
+    const releaseWorkflowPath = '.github/workflows/release.yml'
+
+    test('creates a release workflow', () => {
+      const project = new TestProject({release: false})
+      new ReleaseWorkflow(project.github!)
+      const snapshot = synthSnapshot(project)
+      expect(snapshot).toMatchSnapshot()
+      expect(snapshot[releaseWorkflowPath]).toBeDefined()
+    })
+
+    test('adds a job that creates release', () => {
+      const project = new TestProject({release: false})
+      new ReleaseWorkflow(project.github!)
+      const snapshot = synthSnapshot(project)
+      const workflow = YAML.parse(snapshot[releaseWorkflowPath])
+
+      const createJob = workflow.jobs.create
+      expect(createJob).toBeDefined()
+      expect(createJob.steps[0].uses).toEqual('ottofeller/github-actions/create-release@main')
+    })
+
+    test('allows initialReleaseVersion override', () => {
+      const project = new TestProject({release: false})
+      const initlaReleaseVersion = 'test-version'
+      new ReleaseWorkflow(project.github!, {initlaReleaseVersion})
+      const snapshot = synthSnapshot(project)
+      const workflow = YAML.parse(snapshot[releaseWorkflowPath])
+      expect(workflow.jobs.create.steps[0].with['initial-version']).toEqual(initlaReleaseVersion)
+    })
+
+    test('throws when default release workflow is not disabled', () => {
+      const project = new TestProject()
+      expect(() => new ReleaseWorkflow(project.github!)).toThrow()
+    })
+  })
+})
+
+class TestProject extends NodeProject {
+  constructor(options: Partial<NodeProjectOptions> = {}) {
+    super({
+      ...options,
+      name: 'test-project',
+      defaultReleaseBranch: 'main',
+    })
+  }
+}
