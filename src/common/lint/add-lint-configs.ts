0ab9df7862a0b42588498f1dffd3f8029429bb34
RomanVasilyev <roman.vasilev@ottofeller.com>
2023-03-25 22:00:10 +0300

    Remove projenrc from lintPaths and eslint configs from package.json in subprojects

--- src/common/lint/add-lint-configs.ts
+++ src/common/lint/add-lint-configs.ts
@@ -3,11 +3,9 @@ import {NodeProject} from 'projen/lib/javascript'
 export const addLintConfigs = (project: NodeProject, extraEslintConfigs: Array<string> = []): void => {
   project.package.addField('prettier', '@ottofeller/prettier-config-ofmt')
 
-  project.package.addField('eslintConfig', {
-    extends: [
-      '@ottofeller/eslint-config-ofmt/eslint.quality.cjs',
-      '@ottofeller/eslint-config-ofmt/eslint.formatting.cjs',
-      ...extraEslintConfigs,
-    ],
-  })
+  const commonConfigs = project.parent
+    ? [] // NOTE For a subproject we assume the parent is created from one of our templates and thus has the configs.
+    : ['@ottofeller/eslint-config-ofmt/eslint.quality.cjs', '@ottofeller/eslint-config-ofmt/eslint.formatting.cjs']
+
+  project.package.addField('eslintConfig', {extends: [...commonConfigs, ...extraEslintConfigs]})
 }
