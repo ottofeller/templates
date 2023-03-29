0c6a0ae729e7d97984a8513a3a5699ccdf0b3815
RomanVasilyev <roman.vasilev@ottofeller.com>
2023-03-29 21:55:26 +0300

    Fix class order and invalid class names in Home component of nextjs template

--- src/nextjs/assets/src/Home/index.tsx
+++ src/nextjs/assets/src/Home/index.tsx
@@ -3,14 +3,14 @@ import {memo} from 'react'
 
 export const Home = memo(function Home(props: {cookies?: Record<string, string>}) {
   return (
-    <div className="grid gap-y-4 place-content-center place-items-center min-h-screen">
-      <h2 className="text-24">It works!</h2>
+    <div className="grid min-h-screen place-content-center place-items-center gap-y-4">
+      <h2 className="text-2xl">It works!</h2>
       <nav className="grid gap-y-2">
-        <Link className="text-2196f3 hover:opacity-75 transition-opacity" href="/">
+        <Link className="text-blue-500 transition-opacity hover:opacity-75" href="/">
           Home
         </Link>
 
-        <Link className="text-2196f3 hover:opacity-75 transition-opacity" href="/post">
+        <Link className="text-blue-500 transition-opacity hover:opacity-75" href="/post">
           Blog
         </Link>
       </nav>
