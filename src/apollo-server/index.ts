d57b698a12f140d56ebe8d64527a0618022db8ee
Aleksandr Ivanenko <alex.ivanenko@ottofeller.com>
2023-05-19 20:43:01 +0400

    pla-223 Upgrade apollo server to v4

--- src/apollo-server/index.ts
+++ src/apollo-server/index.ts
@@ -65,12 +65,13 @@ export class OttofellerApolloServerProject extends TypeScriptAppProject {
 
     // ANCHOR Add required dependencies
     this.addDeps(
-      'apollo-server@3.7.0',
+      '@apollo/server@4.7.1',
       'axios@0.27.2',
       'dd-trace@2.7.1',
-      'dotenv@16.0.0',
+      'dotenv@16.0.3',
       'esbuild@0.14.39',
-      'graphql@16.5.0',
+      'graphql@16.6.0',
+      'graphql-tag@2.12.6',
       'source-map-support@0.5.21',
       'yup@0.32.11',
       '@graphql-tools/merge@8.2.11',
