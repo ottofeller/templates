948fa9e202a60bf5c9f06d384c0c05e50277eb39
RomanVasilyev <roman.vasilev@ottofeller.com>
2023-05-26 20:30:06 +0300

    PLA-226 Remove no-relative-parent-imports rule from job.ts
    The import has leaked from another PR so need to remove it here

--- src/common/github/jobs/job.ts
+++ src/common/github/jobs/job.ts
@@ -1,4 +1,3 @@
-/* eslint-disable import/no-relative-parent-imports -- types are not bundled and therefore are not correctly exported with path aliases */
 import * as projen from 'projen'
 import type {Job, JobStep} from 'projen/lib/github/workflows-model'
 import type {MaybePlural} from '../../MaybePlural'
