18be1665eb9033529267369fbe9427d7c66ec854
RomanVasilyev <roman.vasilev@ottofeller.com>
2023-06-28 17:43:09 +0300

    PLA-239 Create SST project template

---
+++ src/sst/__tests__/index.ts
@@ -0,0 +1,107 @@
+import {execSync} from 'child_process'
+import {synthSnapshot} from 'projen/lib/util/synth'
+import {OttofellerSSTProject, OttofellerSSTProjectOptions} from '..'
+
+jest.mock('child_process')
+
+describe('SST template', () => {
+  test('sets defaults', () => {
+    const project = new TestSSTProject()
+    const snapshot = synthSnapshot(project)
+    expect(snapshot).toMatchSnapshot()
+    const {devDependencies} = snapshot['package.json']
+    expect(devDependencies).toHaveProperty('sst')
+    expect(devDependencies).toHaveProperty('aws-cdk-lib')
+    expect(devDependencies).toHaveProperty('constructs')
+  })
+
+  describe('has husky', () => {
+    const commitMsgFileName = '.husky/commit-msg'
+
+    test('omitted by default', () => {
+      const project = new TestSSTProject()
+      const snapshot = synthSnapshot(project)
+      expect(snapshot['package.json'].devDependencies).not.toHaveProperty('husky')
+      expect(snapshot[commitMsgFileName]).not.toBeDefined()
+    })
+
+    test('set up if enabled with hasGitHooks option', () => {
+      const project = new TestSSTProject({hasGitHooks: true})
+      const snapshot = synthSnapshot(project)
+      expect(snapshot['package.json'].devDependencies).toHaveProperty('husky')
+      expect(snapshot[commitMsgFileName]).toBeDefined()
+    })
+  })
+
+  describe('sets initial release version', () => {
+    test('to 0.0.1 by default', () => {
+      const defaultVersion = '0.0.1'
+      const project = new TestSSTProject()
+      const snapshot = synthSnapshot(project)
+      expect(snapshot['.github/workflows/release.yml']).toContain(`initial-version: ${defaultVersion}`)
+    })
+
+    test('to the value provided', () => {
+      const initialReleaseVersion = '1.2.3'
+      const project = new TestSSTProject({initialReleaseVersion})
+      const snapshot = synthSnapshot(project)
+      expect(snapshot['.github/workflows/release.yml']).toContain(`initial-version: ${initialReleaseVersion}`)
+    })
+  })
+
+  test('has prettier and eslint configs', () => {
+    const project = new TestSSTProject()
+    const snapshot = synthSnapshot(project)
+    expect(snapshot['.prettierrc.json']).toBeDefined()
+    expect(snapshot['.eslintrc.json']).toBeDefined()
+  })
+
+  describe('has default test workflow', () => {
+    test('included by default', () => {
+      const project = new TestSSTProject()
+      const snapshot = synthSnapshot(project)
+      expect(snapshot['.github/workflows/test.yml']).toBeDefined()
+      expect(snapshot['.github/workflows/test.yml']).toMatchSnapshot()
+    })
+
+    test('excluded if opted out', () => {
+      const project = new TestSSTProject({hasDefaultGithubWorkflows: false})
+      const snapshot = synthSnapshot(project)
+      expect(snapshot['.github/workflows/test.yml']).not.toBeDefined()
+    })
+  })
+
+  test('includes VSCode settings', () => {
+    const project = new TestSSTProject()
+    const snapshot = synthSnapshot(project)
+    expect(snapshot['.vscode/settings.json']).toBeDefined()
+  })
+
+  test('has gitignore file extended', () => {
+    const project = new TestSSTProject()
+    const snapshot = synthSnapshot(project)
+    expect(snapshot['.gitignore']).toContain('.sst')
+    expect(snapshot['.gitignore']).toContain('.build')
+    expect(snapshot['.gitignore']).toContain('.open-next')
+  })
+
+  test('formats ".projenrc.ts" file after synthesis', () => {
+    // eslint-disable-next-line @typescript-eslint/consistent-type-assertions -- TS is not aware of the Jest mock, thus casting is needed
+    const mockedExecSync = execSync as unknown as jest.Mock<Buffer, [string]>
+    const project = new TestSSTProject()
+    project.postSynthesize()
+    expect(mockedExecSync).toBeCalledTimes(2)
+    expect(mockedExecSync).toBeCalledWith('prettier --write .projenrc.ts')
+    expect(mockedExecSync).toBeCalledWith('eslint --fix .projenrc.ts')
+  })
+})
+
+class TestSSTProject extends OttofellerSSTProject {
+  constructor(options: Partial<OttofellerSSTProjectOptions> = {}) {
+    super({
+      ...options,
+      name: 'test-sst-project',
+      defaultReleaseBranch: 'main',
+    })
+  }
+}
---
+++ src/sst/index.ts
@@ -0,0 +1,115 @@
+import {execSync} from 'child_process'
+import * as path from 'path'
+import {SampleFile} from 'projen'
+import {DependabotScheduleInterval} from 'projen/lib/github'
+import {NodePackageManager} from 'projen/lib/javascript'
+import {TypeScriptAppProject, TypeScriptProjectOptions} from 'projen/lib/typescript'
+import {addHusky, extendGitignore, WithGitHooks} from '../common/git'
+import {PullRequestTest, ReleaseWorkflow, WithDefaultWorkflow} from '../common/github'
+import {addLinters, WithCustomLintPaths} from '../common/lint'
+import {addVsCode} from '../common/vscode-settings'
+
+export interface OttofellerSSTProjectOptions
+  extends TypeScriptProjectOptions,
+    WithDefaultWorkflow,
+    WithCustomLintPaths,
+    WithGitHooks {
+  /**
+   * The base version of the very first release.
+   *
+   * @default 0.0.1
+   */
+  readonly initialReleaseVersion?: string
+}
+
+/**
+ * SST template.
+ *
+ * @pjid ottofeller-sst
+ */
+export class OttofellerSSTProject extends TypeScriptAppProject {
+  constructor(options: OttofellerSSTProjectOptions) {
+    super({
+      // Default options
+      packageManager: options.packageManager ?? NodePackageManager.NPM,
+      tsconfig: {compilerOptions: {paths: {'*': ['./src/*']}, target: 'es6'}},
+      dependabot: (options.github ?? true) && (options.dependabot ?? true),
+      dependabotOptions: {scheduleInterval: DependabotScheduleInterval.WEEKLY},
+
+      // In case Github is enabled remove all default stuff.
+      githubOptions: {mergify: false, pullRequestLint: false},
+      buildWorkflow: false,
+      release: false,
+      depsUpgrade: false,
+      pullRequestTemplate: false,
+
+      // Allow changing default options
+      ...options,
+
+      // Non-changeable options
+      defaultReleaseBranch: 'main',
+      eslint: false,
+      jest: false,
+      name: 'sst',
+      projenrcJs: false,
+      projenrcTs: true,
+      sampleCode: false,
+    })
+
+    // ANCHOR Install dependencies
+    this.addDevDeps('sst', 'aws-cdk-lib', 'constructs')
+
+    // ANCHOR Setup tasks
+    this.removeTask('build')
+    this.removeTask('compile')
+    this.removeTask('install')
+    this.removeTask('install:ci')
+    this.removeTask('package')
+    this.removeTask('post-compile')
+    this.removeTask('pre-compile')
+    this.removeTask('watch')
+    this.addTask('dev', {exec: 'sst dev'})
+    this.addTask('build', {exec: 'sst build'})
+    this.addTask('deploy', {exec: 'sst deploy'})
+    this.addTask('remove', {exec: 'sst remove'})
+    this.addTask('console', {exec: 'sst console'})
+
+    // ANCHOR Setup git hooks with Husky
+    if (options.hasGitHooks ?? false) {
+      addHusky(this, options)
+    }
+
+    // ANCHOR ESLint and prettier setup
+    const lintPaths = options.lintPaths ?? ['.projenrc.ts', 'src', 'sst.config.ts']
+    addLinters({project: this, lintPaths})
+
+    // ANCHOR Github
+    const hasDefaultGithubWorkflows = options.hasDefaultGithubWorkflows ?? true
+
+    if (hasDefaultGithubWorkflows && this.github) {
+      const initialReleaseVersion = options.initialReleaseVersion ?? '0.0.1'
+      new ReleaseWorkflow(this.github, {initialReleaseVersion})
+      PullRequestTest.addToProject(this, {...options, lighthouse: false})
+    }
+
+    // ANCHOR VSCode settings
+    addVsCode(this)
+
+    // ANCHOR gitignore
+    extendGitignore(this)
+    extendGitignore(this, ['node_modules', '.sst', '.build', '.open-next'])
+
+    // ANCHOR SST config
+    const assetsDir = path.join(__dirname, '..', '..', 'src/sst/assets')
+    new SampleFile(this, 'sst.config.ts', {sourcePath: path.join(assetsDir, 'sst.config.ts.sample')})
+  }
+
+  postSynthesize(): void {
+    /*
+     * NOTE: The `.projenrc.ts` file is created by projen and its formatting is not controlled.
+     * Therefore an additional formatting step is required after project initialization.
+     */
+    execSync('prettier --write .projenrc.ts')
+    execSync('eslint --fix .projenrc.ts')
+  }
+}
