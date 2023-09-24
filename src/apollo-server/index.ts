0186e8ff236307e8da4aa453c23c6265d81529aa
Artem Rudenko <gvidon@ottofeller.com>
2023-09-24 09:21:30 +0200

    Clean up comments

--- src/apollo-server/index.ts
+++ src/apollo-server/index.ts
@@ -81,8 +81,8 @@ export class OttofellerApolloServerProject extends TypeScriptAppProject {
     this.package.addField('type', 'module')
 
     /*
-     * ANCHOR Clean off the projen tasks and if needed replace them with regular npm scripts.
-     * NOTE This way we ensure smooth ejection experience with all the commands visible in package.json
+     * Clean off the projen tasks and if needed replace them with regular npm scripts.
+     * This way we ensure smooth ejection experience with all the commands visible in package.json
      * and no need to keep the projen task runner within an ejected project.
      */
     this.removeTask('build')
@@ -171,7 +171,7 @@ export class OttofellerApolloServerProject extends TypeScriptAppProject {
 
   postSynthesize(): void {
     /*
-     * NOTE: The `.projenrc.ts` file is created by projen and its formatting is not controlled.
+     * The `.projenrc.ts` file is created by projen and its formatting is not controlled.
      * Therefore an additional formatting step is required after project initialization.
      */
     execSync('prettier --write .projenrc.mjs')
