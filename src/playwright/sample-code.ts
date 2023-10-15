0b35258c9979ea05db99ca110a4fd0195a1d07ca
RomanVasilyev <roman.vasilev@ottofeller.com>
2023-10-15 13:14:28 +0300

    PLA-271 Move env file into sample code
    The .env.development file is currently not deletable and in order to stop its generation after deletion we need to hide it behind an option - sampleCode seems like the good fit.

--- src/playwright/sample-code.ts
+++ src/playwright/sample-code.ts
@@ -1,5 +1,5 @@
 import * as path from 'path'
-import * as projen from 'projen'
+import {SampleFile} from 'projen'
 import {OttofellerPlaywrightProject, OttofellerPlaywrightProjectOptions} from '.'
 
 /**
@@ -14,51 +14,21 @@ export function sampleCode(
     return
   }
 
-  new projen.SampleFile(project, 'common/index.ts', {
-    sourcePath: path.join(assetsDir, 'common/index.ts.sample'),
-  })
+  new SampleFile(project, '.env.development', {sourcePath: path.join(assetsDir, '.env.development')})
 
-  new projen.SampleFile(project, 'common/test.ts', {
-    sourcePath: path.join(assetsDir, 'common/test.ts.sample'),
-  })
+  new SampleFile(project, 'common/index.ts', {sourcePath: path.join(assetsDir, 'common/index.ts.sample')})
+  new SampleFile(project, 'common/test.ts', {sourcePath: path.join(assetsDir, 'common/test.ts.sample')})
+  new SampleFile(project, 'common/enums/user.ts', {sourcePath: path.join(assetsDir, 'common/enums/user.ts')})
+  new SampleFile(project, 'common/enums/index.ts', {sourcePath: path.join(assetsDir, 'common/enums/index.ts.sample')})
 
-  new projen.SampleFile(project, 'common/enums/user.ts', {
-    sourcePath: path.join(assetsDir, 'common/enums/user.ts'),
-  })
+  new SampleFile(project, 'data/index.ts', {sourcePath: path.join(assetsDir, 'data/index.ts')})
+  new SampleFile(project, 'data/errors.json', {sourcePath: path.join(assetsDir, 'data/errors.json')})
 
-  new projen.SampleFile(project, 'common/enums/index.ts', {
-    sourcePath: path.join(assetsDir, 'common/enums/index.ts.sample'),
-  })
+  new SampleFile(project, 'pages/index.ts', {sourcePath: path.join(assetsDir, 'pages/index.ts.sample')})
+  new SampleFile(project, 'pages/base.ts', {sourcePath: path.join(assetsDir, 'pages/base.ts.sample')})
+  new SampleFile(project, 'pages/index.ts', {sourcePath: path.join(assetsDir, 'pages/index.ts.sample')})
+  new SampleFile(project, 'pages/sign-in.ts', {sourcePath: path.join(assetsDir, 'pages/sign-in.ts.sample')})
+  new SampleFile(project, 'pages/products.ts', {sourcePath: path.join(assetsDir, 'pages/products.ts.sample')})
 
-  new projen.SampleFile(project, 'data/index.ts', {
-    sourcePath: path.join(assetsDir, 'data/index.ts'),
-  })
-
-  new projen.SampleFile(project, 'data/errors.json', {
-    sourcePath: path.join(assetsDir, 'data/errors.json'),
-  })
-
-  new projen.SampleFile(project, 'pages/index.ts', {
-    sourcePath: path.join(assetsDir, 'pages/index.ts.sample'),
-  })
-
-  new projen.SampleFile(project, 'pages/base.ts', {
-    sourcePath: path.join(assetsDir, 'pages/base.ts.sample'),
-  })
-
-  new projen.SampleFile(project, 'pages/index.ts', {
-    sourcePath: path.join(assetsDir, 'pages/index.ts.sample'),
-  })
-
-  new projen.SampleFile(project, 'pages/sign-in.ts', {
-    sourcePath: path.join(assetsDir, 'pages/sign-in.ts.sample'),
-  })
-
-  new projen.SampleFile(project, 'pages/products.ts', {
-    sourcePath: path.join(assetsDir, 'pages/products.ts.sample'),
-  })
-
-  new projen.SampleFile(project, 'specs/auth.spec.ts', {
-    sourcePath: path.join(assetsDir, 'specs/auth.spec.ts.sample'),
-  })
+  new SampleFile(project, 'specs/auth.spec.ts', {sourcePath: path.join(assetsDir, 'specs/auth.spec.ts.sample')})
 }
