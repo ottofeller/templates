dd117c4ce9245638ece327e62fa8998accf9f3e6
mishayouknowme <michael.popov@ottofeller.com>
2023-12-13 14:19:07 +0100

    PLA-256 Add tasks for remove

--- src/backend-test/index.ts
+++ src/backend-test/index.ts
@@ -1,4 +1,3 @@
-import {execSync} from 'child_process'
 import * as path from 'path'
 import {NodePackageManager} from 'projen/lib/javascript'
 import {TypeScriptProject, TypeScriptProjectOptions} from 'projen/lib/typescript'
@@ -75,17 +74,46 @@ export class OttofellerBackendTestProject extends TypeScriptProject implements I
       pullRequestTemplate: false,
     })
 
+    // ANCHOR Source code
     const assetsDir = path.join(__dirname, '..', '..', 'src/backend-test/assets')
     sampleCode(this, options, assetsDir)
+
+    // // ANCHOR playwright config
+    // new SampleFile(this, 'playwright.config.ts', {sourcePath: path.join(assetsDir, 'playwright.config.ts.sample')})
 
     this.addScripts({
       test: 'jest --detectOpenHandles',
     })
+
+    /*
+     * Clean off the projen tasks and if needed replace them with regular npm scripts.
+     * This way we ensure smooth ejection experience with all the commands visible in package.json
+     * and no need to keep the projen task runner within an ejected project.
+     */
+    const tasksToRemove = [
+      'build',
+      'bump',
+      'clobber',
+      'compile',
+      'package',
+      'post-compile',
+      'post-upgrade',
+      'pre-compile',
+      'release',
+      'test',
+      // eslint-disable-next-line @cspell/spellchecker -- the word is used once, so no need to add it to the dictionary
+      'unbump',
+      'upgrade',
+      'watch',
+      'projen',
+    ]
+
+    tasksToRemove.forEach(this.removeTask.bind(this))
   }
 
   postSynthesize(): void {
-    execSync('prettier --write .projenrc.ts')
-    execSync('eslint --fix .projenrc.ts')
+    // execSync('prettier --write .projenrc.ts')
+    // execSync('eslint --fix .projenrc.ts')
 
     collectTelemetry(this)
   }
