43ba515c66198b9aaa3c2432aa9341c312004246
mishayouknowme <michael.popov@ottofeller.com>
2023-12-14 15:59:29 +0100

    PLA-256 Add test samples

--- src/backend-test/sample-code.ts
+++ src/backend-test/sample-code.ts
@@ -12,4 +12,13 @@ export function sampleCode(
   }
 
   new SampleFile(project, '.env.development', {sourcePath: path.join(assetsDir, '.env.development')})
+
+  new SampleFile(project, 'api/auth/index.ts', {sourcePath: path.join(assetsDir, 'api/auth/index.ts.sample')})
+  new SampleFile(project, 'api/auth/request.ts', {sourcePath: path.join(assetsDir, 'api/auth/request.ts.sample')})
+  new SampleFile(project, 'api/auth/response.ts', {sourcePath: path.join(assetsDir, 'api/auth/response.ts.sample')})
+
+  new SampleFile(project, 'common/client.ts', {sourcePath: path.join(assetsDir, 'common/client.ts.sample')})
+  new SampleFile(project, 'common/index.ts', {sourcePath: path.join(assetsDir, 'common/index.ts.sample')})
+
+  new SampleFile(project, 'tests/auth.spec.ts', {sourcePath: path.join(assetsDir, 'tests/auth.spec.ts.sample')})
 }
