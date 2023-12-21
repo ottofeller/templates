da96598a57de004748fb068abd251a93531c7508
mishayouknowme <michael.popov@ottofeller.com>
2023-12-21 16:45:08 +0100

    PLA-256 Add unit test for gql

--- src/backend-test/__tests__/index.ts
+++ src/backend-test/__tests__/index.ts
@@ -7,6 +7,27 @@ describe('Backend-test template', () => {
     const snapshot = synthSnapshot(project)
     expect(snapshot).toMatchSnapshot()
   })
+
+  describe('has GraphQL', () => {
+    test('enabled by default', () => {
+      const project = new TestBackendTestProject()
+      const snapshot = synthSnapshot(project)
+      expect(snapshot['package.json'].dependencies).toHaveProperty('@apollo/client')
+      expect(snapshot['package.json'].dependencies).toHaveProperty('graphql')
+      expect(snapshot['package.json'].scripts).toHaveProperty('gql-to-ts')
+      expect(snapshot['codegen.ts']).toBeDefined()
+    })
+
+    test('disabled with an option', () => {
+      const project = new TestBackendTestProject({isGraphqlEnabled: false})
+      const snapshot = synthSnapshot(project)
+      expect(snapshot['package.json'].dependencies).not.toHaveProperty('@apollo/client')
+      expect(snapshot['package.json'].dependencies).not.toHaveProperty('graphql')
+      expect(snapshot['package.json'].scripts).not.toHaveProperty('generate-graphql-schema')
+      expect(snapshot['package.json'].scripts).not.toHaveProperty('gql-to-ts')
+      expect(snapshot['codegen.ts']).not.toBeDefined()
+    })
+  })
 })
 
 class TestBackendTestProject extends OttofellerBackendTestProject {
