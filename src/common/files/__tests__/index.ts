a471b832c336d3408586529df6850625fad7d8ca
RomanVasilyev <roman.vasilev@ottofeller.com>
2024-01-08 11:39:13 +0300

    PLA-284 Expand test coverage for AssetFile
    Add a test for correct handling of array templates.
    Add a test for formatting being skipped for non-TS/JS files.

--- src/common/files/__tests__/index.ts
+++ src/common/files/__tests__/index.ts
@@ -28,15 +28,39 @@ describe('AssetFile util', () => {
     expect(asset).toEqual('test asset updated\n')
   })
 
-  test('formats JS/TS code with prettier after template replacement', () => {
+  test('can utilize multiple templates for replacement', () => {
+    const arrayTemplate = [
+      {templateString: '{{REPLACE', replacement: 'updated'},
+      {templateString: 'ME}}', replacement: 'twice'},
+    ]
+
+    const project = new TestProject()
+    new AssetFile(project, 'asset.txt', {sourcePath, template: arrayTemplate})
+    const snapshot = synthSnapshot(project)
+    const asset = snapshot['asset.txt']
+    expect(asset).toBeDefined()
+    expect(asset).toEqual('test asset updated twice\n')
+  })
+
+  describe('prettier formatting after template replacement', () => {
     // eslint-disable-next-line @typescript-eslint/consistent-type-assertions -- TS is not aware of the Jest mock, thus casting is needed
     const mockedFormat = format as unknown as jest.Mock<string, [string, Options]>
     mockedFormat.mockReturnValue('formatted content')
-    const project = new TestProject()
-    new AssetFile(project, 'asset.ts', {sourcePath, template})
-    synthSnapshot(project)
-    expect(mockedFormat).toHaveBeenCalledTimes(1)
-    expect(mockedFormat).toHaveBeenCalledWith('test asset updated\n', expect.any(Object))
+
+    test('applied to JS/TS code', () => {
+      const project = new TestProject()
+      new AssetFile(project, 'asset.ts', {sourcePath, template})
+      synthSnapshot(project)
+      expect(mockedFormat).toHaveBeenCalledTimes(1)
+      expect(mockedFormat).toHaveBeenCalledWith('test asset updated\n', expect.any(Object))
+    })
+
+    test('does not apply to non JS/TS files', () => {
+      const project = new TestProject()
+      new AssetFile(project, 'asset.rs', {sourcePath, template})
+      synthSnapshot(project)
+      expect(mockedFormat).not.toHaveBeenCalled()
+    })
   })
 })
 
